1a08dec9d6b410e6428e52c274ddfb173c7754a7
Vishal-NodeServer <vishal123suthar2@gmail.com>
2024-07-27 17:09:54 +0530

    added by vishal suthar

--- lib/theme/style.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:flutter/material.dart';
-
-// const primaryColorED6E1B = Color.fromARGB(255, 0, 56, 102);
-// const whiteColor = Colors.white;
-// const redColor = Color(0xFFE41D1A);
-// const lightGreyColor = Color(0xFFEDEDED);
