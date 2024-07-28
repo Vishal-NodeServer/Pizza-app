839af6da465cb5248447d36f365462b0605f9b59
Vishal-NodeServer <vishal123suthar2@gmail.com>
2024-07-28 19:32:03 +0530

    image added in login and signup by vishal

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'screens/splash/splash_screen.dart'; // Update with the correct path
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => YourProvider(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
-
-class YourProvider extends ChangeNotifier {}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -19,9 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+      // debugShowCheckedModeBanner: false,
       title: "Pizza&Hungry",
-
       home: SplashScreen(), // SplashScreen will handle navigation to LoginPage
     );
   }
--- lib/screens/auth/sign_up_page.dart
+++ lib/screens/auth/sign_up_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_prime_app/screens/auth/login_page.dart';
 import 'package:food_prime_app/screens/main/main_screen.dart';
-import 'package:food_prime_app/widgets/button_container_widget.dart';
-import 'package:food_prime_app/widgets/form_container_widget.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
@@ -12,88 +10,144 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final TextEditingController username = TextEditingController();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  final TextEditingController phone = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-      child: Column(
+      body: Stack(
         children: [
-          Expanded(
+          // Background image at the bottom
+          Positioned(
+            top: 100,
+            bottom: 100,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/logoutpizza.png', // Path to your image
+              height: 100, // Adjust height as needed
+              width: 50, // Make the image cover the width of the screen
+              fit: BoxFit.cover, // Scale the image to cover the area
+            ),
+          ),
+          // Form container
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(
-                  height: 80,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 80),
+                      const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20),
+                      _buildTextField(
+                        controller: username,
+                        hintText: "Username",
+                      ),
+                      const SizedBox(height: 20),
+                      _buildTextField(
+                        controller: email,
+                        hintText: "Email",
+                      ),
+                      const SizedBox(height: 20),
+                      _buildTextField(
+                        controller: password,
+                        hintText: "Password",
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 20),
+                      _buildTextField(
+                        controller: phone,
+                        hintText: "Phone number",
+                      ),
+                      const SizedBox(height: 30),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => const MainScreen(),
+                              ),
+                              (route) => false,
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Color.fromARGB(255, 0, 56, 102),
+                            padding: const EdgeInsets.symmetric(vertical: 15),
+                            textStyle: const TextStyle(fontSize: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Text("Sign Up"),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
                 ),
-                const Text(
-                  "Sign Up",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Already have an account? ",
+                      style: TextStyle(fontSize: 15, color: Colors.black),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => const LoginPage(),
+                          ),
+                          (route) => false,
+                        );
+                      },
+                      child: const Text(
+                        "Log In",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color.fromARGB(255, 0, 56, 102),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const FormContainerWidget(hintText: "Username"),
-                const SizedBox(
-                  height: 20,
-                ),
-                const FormContainerWidget(hintText: "Email"),
-                const SizedBox(
-                  height: 20,
-                ),
-                const FormContainerWidget(hintText: "Password"),
-                const SizedBox(
-                  height: 20,
-                ),
-                const FormContainerWidget(hintText: "Phone number"),
-                const SizedBox(
-                  height: 20,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                ButtonContainerWidget(
-                  title: "Sign Up",
-                  onTap: () {
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (_) => const MainScreen()),
-                        (route) => false);
-                  },
-                  color: Colors.yellow,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
+                const SizedBox(height: 20),
               ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Already have an account? ",
-                style: TextStyle(fontSize: 15),
-              ),
-              GestureDetector(
-                  onTap: () {
-                    Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (_) => const LoginPage()),
-                        (route) => false);
-                  },
-                  child: const Text(
-                    "Log In",
-                    style: TextStyle(
-                        fontSize: 15, color: Color.fromARGB(255, 0, 56, 102)),
-                  ))
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
         ],
       ),
-    ));
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String hintText,
+    bool obscureText = false,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        hintText: hintText,
+      ),
+    );
   }
 }
