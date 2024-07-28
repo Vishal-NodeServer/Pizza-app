3269bbdfd51fdf7de6173788b065f8f177a6c4eb
Vishal-NodeServer <vishal123suthar2@gmail.com>
2024-07-28 19:32:03 +0530

    image added in login and signup by vishal

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:food_prime_app/screens/auth/sign_up_page.dart';
 import 'package:food_prime_app/screens/main/main_screen.dart';
 
-import '../../widgets/button_container_widget.dart';
-import '../../widgets/form_container_widget.dart'; // Ensure this import matches your project structure
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -13,112 +10,154 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-        child: Column(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 140),
-                  const Text(
-                    "Log In",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              // Image widget at the top
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(bottom: 30),
+                child: AspectRatio(
+                  aspectRatio: 1.2, // Adjust aspect ratio to fit your design
+                  child: Image.asset(
+                    'assets/chef.png', // Path to your image
+                    fit: BoxFit
+                        .cover, // Scale the image to fit within its container
                   ),
-                  const SizedBox(height: 20),
-                  const FormContainerWidget(hintText: "Email or Username"),
-                  const SizedBox(height: 20),
-                  const FormContainerWidget(hintText: "Password"),
-                  const SizedBox(height: 20),
-                  const SizedBox(height: 20),
-                  ButtonContainerWidget(
-                    title: "Log In",
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 20),
+                    const Text(
+                      "Log In",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 20),
+                    _buildTextField(
+                      controller: email,
+                      hintText: "Email or Username",
+                    ),
+                    const SizedBox(height: 20),
+                    _buildTextField(
+                      controller: password,
+                      hintText: "Password",
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MainScreen(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor:
+                              const Color.fromARGB(255, 0, 56, 102),
+                          padding: EdgeInsets.symmetric(
+                              vertical:
+                                  MediaQuery.of(context).size.height * 0.02),
+                          textStyle: TextStyle(
+                              fontSize:
+                                  MediaQuery.of(context).textScaleFactor * 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: const Text("Log In"),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Expanded(child: divider()),
+                        const SizedBox(width: 10),
+                        const Text(
+                          "OR",
+                          style: TextStyle(color: Colors.black, fontSize: 16),
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(child: divider()),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account? ",
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  GestureDetector(
                     onTap: () {
-                      Navigator.pushReplacement(
+                      Navigator.pushAndRemoveUntil(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => const MainScreen(),
+                          builder: (_) => const SignUpPage(),
                         ),
+                        (route) => false,
                       );
                     },
-                    color: Colors.yellow,
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          width: double.infinity,
-                          height: 1,
-                          color: Colors.black,
-                        ),
+                    child: const Text(
+                      "Create account",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Color.fromARGB(255, 0, 56, 102),
                       ),
-                      Expanded(
-                        child: Container(
-                          width: double.infinity,
-                          height: 1,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Don't have an account? ",
-                  style: TextStyle(fontSize: 15),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => const SignUpPage(),
-                      ),
-                      (route) => false,
-                    );
-                  },
-                  child: const Text(
-                    "Create account",
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Color(
-                          0xFFED6E1B), // Replace with primaryColorED6E1B if defined elsewhere
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _optionSignIn({required IconData iconData, required Color color}) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 10),
-      width: 40,
-      height: 40,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: color,
-      ),
-      child: Center(
-        child: Icon(
-          iconData,
-          color: Colors.white, // Use a defined color if available
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String hintText,
+    bool obscureText = false,
+  }) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
         ),
+        hintText: hintText,
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
       ),
+    );
+  }
+
+  Widget divider() {
+    return Container(
+      height: 1,
+      color: Colors.black,
     );
   }
 }
