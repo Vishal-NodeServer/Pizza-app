080542b931fe9ac0558759a3191ae8af52a5ed55
jayveer-glitch <jayveersathvara744@gmail.com>
2024-09-05 22:34:28 +0530

    image added

--- lib/data/food_prime_data.dart
+++ lib/data/food_prime_data.dart
@@ -2,52 +2,55 @@
 
 final BURGER_SPECIAL_CATEGORY_LIST = [
   {
-    "image": "loggo.png", // Replaced
-    "title": "Lentil and Mushroom Burger",
+    "image": "7cheese.jpg", // Replaced
+    "title": "7cheese Pizza",
     "rating": "4.8"
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "Stuff Bean Burger",
+    "image": "broccoli.jpg", // Replaced
+    "title": "Broccoli Pizza",
     "rating": "5.0"
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "Potato Corn Burger",
+    "image": "cheeseblast.jpg", // Replaced
+    "title": "CheeseBlast pizza",
     "rating": "3.3"
   },
-  {"image": "loggo.png", "title": "Pizza Burger", "rating": "4.5"},
 ];
 
 final BURGER_POPULAR_CATEGORY_LIST = [
   {
-    "image": "loggo.png", // Replaced
-    "title": "Beef Burger",
+    "image": "corn.jpg", // Replaced
+    "title": "Corn Pizza",
     "rating": "4.9"
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "EIK Burger",
+    "image": "margarita.jpg", // Replaced
+    "title": "Margarita Pizza",
     "rating": "3.0"
   },
-  {"image": "loggo.png", "title": "Turkey Burger", "rating": "3.9"},
-  {"image": "loggo.png", "title": "Vaggie Burger", "rating": "2.8"},
-  {"image": "loggo.png", "title": "Bison Burger", "rating": "2.0"},
+  {"image": "mushroom.jpg", "title": "Mushrom pizza", "rating": "3.9"},
+  {"image": "paneer.jpg", "title": "Paneer Pizza", "rating": "2.8"},
+  {"image": "tomato.png", "title": "Tomato pizza", "rating": "2.0"},
+  {"image": "veggie.jpg", "title": "Veggie Pizza", "rating": "4.5"},
 ];
 
 final PIZZA_SPECIAL_CATEGORY_LIST = [
-  {"image": "loggo.png", "title": "Neapolitan Pizza", "rating": "4.9"},
+  {"image": "combo_1.jpg", "title": "Neapolitan Pizza", "rating": "4.9"},
   {"image": "loggo.png", "title": "Chicago Pizza", "rating": "5.0"},
   {"image": "loggo.png", "title": "New York-Style Pizza", "rating": "5.0"},
   {"image": "loggo.png", "title": "Sicilian Pizza", "rating": "4.2"},
 ];
 
 final PIZZA_POPULAR_CATEGORY_LIST = [
-  {"image": "loggo.png", "title": "Greek Pizza", "rating": "4.2"},
-  {"image": "loggo.png", "title": "California Pizza", "rating": "3.7"},
-  {"image": "loggo.png", "title": "Detroit Pizza", "rating": "4.9"},
-  {"image": "loggo.png", "title": "St. Louis Pizza", "rating": "3.8"},
-  {"image": "loggo.png", "title": "Meat Pizza", "rating": "5.0"},
+  {"image": "combo_1.jpg", "title": "Greek Pizza", "rating": "4.2"},
+  {"image": "combo_2.jpg", "title": "California Pizza", "rating": "3.7"},
+  {"image": "combo_3.jpg", "title": "Detroit Pizza", "rating": "4.9"},
+  {"image": "combo_4.jpg", "title": "St. Louis Pizza", "rating": "3.8"},
+  {"image": "combo_5.jpg", "title": "Meat Pizza", "rating": "5.0"},
+  {"image": "combo_6.jpg", "title": "Meat Pizza", "rating": "5.0"},
+  {"image": "combo_7.jpg", "title": "Meat Pizza", "rating": "5.0"},
+  {"image": "combo_8.jpg", "title": "Meat Pizza", "rating": "5.0"},
 ];
 
 final SANDWICH_SPECIAL_CATEGORY_LIST = [
@@ -75,28 +78,28 @@ final SANDWICH_SPECIAL_CATEGORY_LIST = [
 
 final SANDWICH_POPULAR_CATEGORY_LIST = [
   {
-    "image": "loggo.png", // Replaced
-    "title": "Grilled Sandwich",
+    "image": "cocacola.jpg", // Replaced
+    "title": "Coca Cola",
     "rating": "4.8"
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "Nutella Sandwich",
+    "image": "monster.jpg", // Replaced
+    "title": "Monster energy",
     "rating": "4.7"
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "Ham Sandwich",
+    "image": "mountaindew.jpg", // Replaced
+    "title": "Mountaindew",
     "rating": "5.0"
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "Grilled Cheese",
+    "image": "pepsi.jpg", // Replaced
+    "title": "Pepsi",
     "rating": "2.9"
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "Egg Sandwich",
+    "image": "sprite.jpg", // Replaced
+    "title": "sprite",
     "rating": "4.3"
   },
 ];
