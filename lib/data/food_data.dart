445e4d7dc06d91330d2b70dcf87e4903161a3ce3
jayveer-glitch <jayveersathvara744@gmail.com>
2024-09-17 00:08:43 +0530

    image added

--- lib/data/food_data.dart
+++ lib/data/food_data.dart
@@ -17,69 +17,93 @@ final PIZZA_POPULAR_LIST = [
     "rating": "3.3",
     "price": "599" // Example price
   },
+  {
+    "image": "spicy.jpg", // Replaced
+    "title": "Spicy pizza",
+    "rating": "3.9",
+    "price": "299" // Example price
+  },
+  {
+    "image": "paneer.jpg", // Replaced
+    "title": "paneer pizza",
+    "rating": "3.5",
+    "price": "399" // Example price
+  },
+  {
+    "image": "mushroom.jpg", // Replaced
+    "title": "Mushroom pizza",
+    "rating": "4.3",
+    "price": "599" // Example price
+  },
+  {
+    "image": "tomato.jpg", // Replaced
+    "title": "Tomato pizza",
+    "rating": "3.6",
+    "price": "199" // Example price
+  },
+  {
+    "image": "veggie.jpg", // Replaced
+    "title": "Veggie pizza",
+    "rating": "3.6",
+    "price": "199" // Example price
+  },
+  {
+    "image": "puffpizza.jpg", // Replaced
+    "title": "Puffpizza",
+    "rating": "3.6",
+    "price": "199" // Example price
+  },
 ];
 
 final PIZZA_SPECIAL_LIST = [
   {
-    "image": "corn.jpg", // Replaced
-    "title": "Corn Pizza",
+    "image": "combo_5.jpg", // Replaced
+    "title": "pizza,garlic bread with chocolava cake",
     "rating": "4.9",
     "price": "519" // Example price
   },
   {
-    "image": "margarita.jpg", // Replaced
-    "title": "Margarita Pizza",
+    "image": "combo_6.jpg", // Replaced
+    "title": "Double Pizza with garlic bread",
     "rating": "3.0",
     "price": "459" // Example price
   },
   {
-    "image": "mushroom.jpg",
-    "title": "Mushroom pizza",
+    "image": "combo_7.jpg",
+    "title": "2pizza with garlic bread ",
     "rating": "3.9",
     "price": "489" // Example price
   },
   {
-    "image": "paneer.jpg",
-    "title": "Paneer Pizza",
+    "image": "combo_8.jpg",
+    "title": "2pizza with 2 garlic bread",
     "rating": "2.8",
     "price": "529" // Example price
-  },
-  {
-    "image": "tomato.png",
-    "title": "Tomato pizza",
-    "rating": "2.0",
-    "price": "479" // Example price
-  },
-  {
-    "image": "veggie.jpg",
-    "title": "Veggie Pizza",
-    "rating": "4.5",
-    "price": "499" // Example price
   },
 ];
 
 final PIZZA_SPECIAL_COMBO_LIST = [
   {
     "image": "combo_1.jpg",
-    "title": "Neapolitan Pizza",
+    "title": "Burger & fries",
     "rating": "4.9",
     "price": "699" // Example price
   },
   {
-    "image": "loggo.png",
-    "title": "Chicago Pizza",
+    "image": "combo_2.jpg",
+    "title": "Double pizza with 2 dips",
     "rating": "5.0",
     "price": "749" // Example price
   },
   {
-    "image": "loggo.png",
-    "title": "New York-Style Pizza",
+    "image": "combo_3.jpg",
+    "title": "4 pizza combo",
     "rating": "5.0",
     "price": "729" // Example price
   },
   {
-    "image": "loggo.png",
-    "title": "Sicilian Pizza",
+    "image": "combo_4.jpg",
+    "title": "Pizza with garlic bread",
     "rating": "4.2",
     "price": "699" // Example price
   },
@@ -87,79 +111,103 @@ final PIZZA_SPECIAL_COMBO_LIST = [
 
 final PIZZA_POPULAR_COMBO_LIST = [
   {
-    "image": "combo_1.jpg",
-    "title": "Greek Pizza",
+    "image": "7up.jpg",
+    "title": "7Up soft drink",
     "rating": "4.2",
     "price": "639" // Example price
   },
   {
-    "image": "combo_2.jpg",
+    "image": "mountaindew.jpg",
     "title": "California Pizza",
     "rating": "3.7",
     "price": "649" // Example price
   },
   {
-    "image": "combo_3.jpg",
+    "image": "pepsi.jpg",
     "title": "Detroit Pizza",
     "rating": "4.9",
     "price": "689" // Example price
   },
   {
-    "image": "combo_4.jpg",
+    "image": "fanta.jpg",
     "title": "St. Louis Pizza",
     "rating": "3.8",
     "price": "659" // Example price
-  },
-  {
-    "image": "combo_5.jpg",
-    "title": "Meat Pizza",
-    "rating": "5.0",
-    "price": "699" // Example price
-  },
-  {
-    "image": "combo_6.jpg",
-    "title": "Meat Pizza",
-    "rating": "5.0",
-    "price": "699" // Example price
-  },
-  {
-    "image": "combo_7.jpg",
-    "title": "Meat Pizza",
-    "rating": "5.0",
-    "price": "699" // Example price
-  },
-  {
-    "image": "combo_8.jpg",
-    "title": "Meat Pizza",
-    "rating": "5.0",
-    "price": "699" // Example price
   },
 ];
 
 final BEVERAGE_SPECIAL_LIST = [
   {
-    "image": "loggo.png", // Replaced
-    "title": "Chicken Sandwich",
+    "image": "7up.jpg", // Replaced
+    "title": "7up",
     "rating": "4.2",
     "price": "299" // Example price
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "Egg Sandwich",
+    "image": "cocacola.jpg", // Replaced
+    "title": "Coca cola",
     "rating": "4.5",
     "price": "279" // Example price
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "Seafood Sandwich",
+    "image": "mountaindew.jpg", // Replaced
+    "title": "Mountaindew",
     "rating": "4.2",
     "price": "349" // Example price
   },
   {
-    "image": "loggo.png", // Replaced
-    "title": "Roast Beef Sandwich",
+    "image": "fanta.jpg", // Replaced
+    "title": "Fanta",
     "rating": "5.0",
     "price": "359" // Example price
+  },
+  {
+    "image": "monster.jpg", // Replaced
+    "title": "Monster energy",
+    "rating": "4.7",
+    "price": "129" // Example price
+  },
+  {
+    "image": "sprite.jpg", // Replaced
+    "title": "Sprite",
+    "rating": "4.3",
+    "price": "89" // Example price
+  },
+  {
+    "image": "coczero.jpg", // Replaced
+    "title": "coke zero sugar",
+    "rating": "3.6",
+    "price": "199" // Example price
+  },
+  {
+    "image": "redbull.jpg", // Replaced
+    "title": "Redbull",
+    "rating": "3.6",
+    "price": "199" // Example price
+  },
+  {
+    "image": "orange.jpg", // Replaced
+    "title": "Orange Juice",
+    "rating": "3.6",
+    "price": "199" // Example price
+  },
+  {
+    "image": "watermelon.jpg", // Replaced
+    "title": "Watermelon Juice",
+    "rating": "3.6",
+    "price": "199" // Example price
+  },
+  {
+    "image": "guava.jpg", // Replaced
+    "title": "guava juice",
+    "rating": "3.6",
+    "price": "199" // Example price
+  },
+  {
+    "image": "pineapple.jpg", // Replaced
+    "title": "pineapple juice",
+    "rating": "3.6",
+    "price": "199" // Example price
   },
 ];
 
